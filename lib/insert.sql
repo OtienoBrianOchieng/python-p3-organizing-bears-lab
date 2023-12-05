0e6de30af1a728a2ff1b09da8b763689b0c0d70c
root <root@DESKTOP-UEO62DE.localdomain>
2023-12-05 22:18:10 +0300

    Complete SQL lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Black", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "White", "Mild", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergent Brown", 4, "M", "Spotted", "Rowdy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 20, "M", "Brown", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "F", "Grey", "Outgoing", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "M", "Brown", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "Brown", "Calm", TRUE);
